92c325a8158f6406685ac86160b7261d8391526a
antomereu <antonella.mereu98@tiscali.it>
2018-03-28 12:44:17 +0200

    Prelievi.java+ PrelieviDAO.java ciao da anto

---
+++ AziendaOspedaliera/src/it/gov/giua/database/dao/PrelieviDAO.java
@@ -0,0 +1,62 @@
+package it.gov.giua.database.dao;
+//package it.gov.giua.database.dao.UtenteDAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.Date;
+import java.util.Calendar;
+
+import it.gov.giua.model.Prelievi;
+
+
+public class PrelieviDAO extends BaseDAO {
+
+	public PrelieviDAO() {
+		super();
+
+	}
+
+	public List<Prelievi> getAllPrelievi(String query) {
+		logger.info("Recupero tutti i prelievi");
+		List<Prelievi> Prelievi = new ArrayList<Prelievi>();
+
+		try {
+			ResultSet rs = getDbm().performQuery(query);
+			while (rs.next()) {
+				int id_prelievo = rs.getInt("id_prelievo");
+				Date data_ora_prelievo = rs.getDate("data_ora_prelievo");
+				Date data_ora_fine = rs.getDate("data_ora_fine");
+				String codice_visita = rs.getString("codice_visita");
+				String referto = rs.getString("referto");
+				int dipendenti_id_dipendente = rs.getInt("dipendenti_id_dipendente");
+
+				Prelievi current = new Prelievi( id_prelievo,  data_ora_prelievo,  data_ora_fine ,  codice_visita, dipendenti_id_dipendente);
+				current.setId_prelievo(id_prelievo);
+				Prelievi.add(current);
+			}
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE, "Errore nel recupero dei prelievi ->" + e.getMessage());
+			Prelievi = new ArrayList<Prelievi>();
+		}
+
+		return Prelievi;
+	}
+
+
+	public List<Prelievi> getPrelievoByCodicePrelievi() {
+		return getAllPrelievi("select * from utenti, prelievi where utenti.ID_UTENTE = prelievi.utenti_ID_UTENTE AND prelievi.CODICE_VISITA = 'SALMO';");
+	}
+	/*this.getUtentebyCodFiscale(){
+		super();
+	}*/
+	
+	
+	
+	/*public News getSingleNews(int id) {
+		return getAllNews("select * from news where abilitato = 1 and id ="+id).get(0);
+	}*/
+}
+
---
+++ AziendaOspedaliera/src/it/gov/giua/model/Prelievi.java
@@ -0,0 +1,95 @@
+package it.gov.giua.model;
+import java.util.Date;
+import java.util.Calendar;
+
+public class Prelievi {
+	
+	private int id_prelievo;
+	private Date data_ora_prelievo;
+	private Date data_ora_fine ;
+	private String codice_visita ;
+	private String referto;
+	private int dipendenti_id_dipendente;
+
+	
+	/**
+	 * 
+	 */
+	public Prelievi() {
+		super();
+	}
+	
+	/**
+	 * 
+	 * @param titolo Titolo della news da creare
+	 * @param sottotitolo Sottotitolo della news da creare
+	 * @param testo Testo della news da creare
+	 * @param abilitato Flag che indica se la news è abilitata o meno
+	 * @param tipo Tipo della news: esempio C (corso), G (generica), S (specialistica)
+	 */
+	public Prelievi(int id_prelievo, Date data_ora_prelievo, Date data_ora_fine , String codice_visita,int dipendenti_id_dipendente) {
+		super();
+		this.id_prelievo=id_prelievo;	
+		this.data_ora_prelievo=data_ora_prelievo;
+		this.data_ora_fine=data_ora_fine;
+		this.codice_visita=codice_visita;
+		this.dipendenti_id_dipendente=dipendenti_id_dipendente;
+	}
+
+
+	public int getId_prelievi() {
+		return id_prelievo;
+	}
+	public void setId_prelievo(int id_prelievo) {
+		this.id_prelievo = id_prelievo;
+	}
+	
+
+	public Date getData_ora_prelievo() {
+		return data_ora_prelievo;
+	}
+
+	public Date getData_ora_fine() {
+		return data_ora_fine;
+	}
+
+	public String getCodice_visita() {
+		return codice_visita;
+	}
+
+	public String getReferto() {
+		return referto;
+	}
+
+	public int getDipendenti_id_dipendente() {
+		return dipendenti_id_dipendente;
+	}
+
+	public void setData_ora_prelievo(Date data_ora_prelievo) {
+		this.data_ora_prelievo = data_ora_prelievo;
+	}
+
+	public void setData_ora_fine(Date data_ora_fine) {
+		this.data_ora_fine = data_ora_fine;
+	}
+
+	public void setCodice_visita(String codice_visita) {
+		this.codice_visita = codice_visita;
+	}
+
+	public void setReferto(String referto) {
+		this.referto = referto;
+	}
+
+	public void setDipendenti_id_dipendente(int dipendenti_id_dipendente) {
+		this.dipendenti_id_dipendente = dipendenti_id_dipendente;
+	}
+
+	@Override
+	public String toString() {
+		return "Prelievi [id_prelievo=" + id_prelievo + ", data_ora_prelievo=" + data_ora_prelievo + ", data_ora_fine="
+				+ data_ora_fine + ", codice_visita=" + codice_visita + ", referto=" + referto
+				+ ", dipendenti_id_dipendente=" + dipendenti_id_dipendente + "]";
+	}
+}
+	
