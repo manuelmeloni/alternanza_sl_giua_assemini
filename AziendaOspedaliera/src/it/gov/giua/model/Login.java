9059f6779819857afa033daf4002d5144c8e7797
francescomancosu98 <mancosu.francesco98@gmail.com>
2018-03-28 11:35:08 +0200

    aggiunta la classe Login.java

---
+++ AziendaOspedaliera/src/it/gov/giua/model/Login.java
@@ -0,0 +1,49 @@
+package it.gov.giua.model;
+
+public class Login {
+	private int id;
+	private String nome;
+	private String cognome;
+	private String categoria;
+	private String username;
+	private String password;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
