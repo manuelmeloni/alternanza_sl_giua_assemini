74a20851d98b268c2e9a134ad740627dd8ea3c2a
MatteoTuzi <Matteo.Tuzi00@gmail.com>
2018-03-28 11:56:46 +0200

    Aggiunto: LoginDAO.java; Modificato: LoginChecker.jsp;

--- AziendaOspedaliera/WebContent/LoginChecker.jsp
+++ AziendaOspedaliera/WebContent/LoginChecker.jsp
@@ -3,7 +3,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 
-<main><main role="main"> <%
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Controllo Login</title>
+</head>
+<body>
+	<main><main role="main"> <%
 		//"dao" permette la connessione con il DB
 		LoginDAO dao = new LoginDAO();
 		//Creo un oggetto di tipo login, che ottiene i dati necessari al login
@@ -12,12 +19,5 @@
 		%>
 		
 </main>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Controllo Login</title>
-</head>
-<body>
-	
 </body>
 </html>
---
+++ AziendaOspedaliera/src/it/gov/giua/database/dao/LoginDAO.java
@@ -0,0 +1,37 @@
+package it.gov.giua.database.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import it.gov.giua.model.Login;
+import it.gov.giua.model.News;
+
+public class LoginDAO extends BaseDAO{
+
+	public LoginDAO() {
+		super();
+	}
+	
+	public Login getUser(String query) {
+		logger.info("Recupero tutte le news");
+		try {
+			
+			ResultSet rs = getDbm().performQuery(query);
+				int id = rs.getInt("id");
+				String nome = rs.getString("titolo");
+				String cognome = rs.getString("sottotitolo");
+				int categoria = rs.getInt(1);
+				String username = rs.getString("username");
+				String password= rs.getString("password");
+				
+				Login current = new Login(id,nome,cognome,categoria,username,password);
+				
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE, "Errore nel recupero delle info ->" + e.getMessage());
+		}
+
+	}
+}
