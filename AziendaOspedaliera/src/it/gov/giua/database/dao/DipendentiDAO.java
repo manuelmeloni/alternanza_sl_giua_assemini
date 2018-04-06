1f09c2779fa253e449c87283a48013eeb39aa40f
Alessandro Melis <alessandro.cbr.melis@gmail.com>
2018-04-06 17:53:22 +0200

    Prima bozza DipendentiDAO

--- AziendaOspedaliera/src/it/gov/giua/database/dao/DipendentiDAO.java
+++ AziendaOspedaliera/src/it/gov/giua/database/dao/DipendentiDAO.java
@@ -39,7 +39,7 @@ public class DipendentiDAO extends BaseDAO {
 				String password= rs.getString("password");
 
 				Dipendenti current = new Dipendenti(data_nascita,nome,cognome,codice_fiscale,mail,data_assunzione,data_licenziamento,categoria,username,password);
-				current.setID(ID);
+				current.setId_dipendente(id_dipendente);
 				dip.add(current);
 			}
 		} catch (SQLException e) {
@@ -53,19 +53,73 @@ public class DipendentiDAO extends BaseDAO {
 	
 	
 	public Dipendenti getDipendente(String username, String password) {
+		//ResultSet rs = getDbm().performQuery(query);
 		return (Dipendenti) getAllDipendenti("Select * FROM Dipendenti WHERE password="+password+" username="+username);
 		
 	}
 	
 	public Dipendenti deleteDipendente(String username, String password) {
+		//ResultSet rs = getDbm().performQuery(query);
 		return (Dipendenti) getAllDipendenti("DELETE FROM Dipendenti WHERE password="+password+" username="+username);
 		
 	}
 	
+	
 	public Dipendenti setDipendente(Date data_nascita,String nome, String cognome, String codice_fiscale, String email, Date data_assunzione,Date data_licenziamento,int categoria, String username, String password) {
-		return (Dipendenti) getAllDipendenti("Insert into (DATA_NASCITA,NOME,COGNOME,CODICE_FISCALE,MAIL,DATA_ASSUNZIONE,DATA_LICENZIAMENTO,CATEGORIA,USERNAME,PASSWORD) "
-				+ "values('"+data_nascita+"','"+nome+"','"+cognome+"')");
-		
-		
+		return (Dipendenti) getAllDipendenti("Insert into Dipendenti(DATA_NASCITA,NOME,COGNOME,CODICE_FISCALE,MAIL,DATA_ASSUNZIONE,DATA_LICENZIAMENTO,CATEGORIA,USERNAME,PASSWORD) "
+				+ "values('"+data_nascita+"','"+nome+"','"+cognome+"','"+codice_fiscale+"','"+email+"','"+data_assunzione+"','"+data_licenziamento+"','"+categoria+"','"+username+"','"+password+"')");
 	}
+	
+	public Dipendenti getUtentiReparto(String reparto/*da cambiare il nome reparto*/) {
+		return (Dipendenti) getAllDipendenti("Select pazienti.* "
+				+ "FROM Dipendenti,utenti,/***tabellaassociativa***/,reparto  "
+				+ "WHERE Dipendenti.ID_REPARTO=reparto.ID_REPARTO AND "
+				+ "/***tabellaassociativa***/.ID_REPARTO=reparto.ID_REPARTO AND "
+				+ "Utenti.ID_UTENTE=/***tabellaassociativa***/.ID_UTENTI AND "
+				+ "reparto.NOME='"+reparto+"' AND  "
+				+ "Dipendenti.categoria='medico' "	
+				+ "GROUP BY reparto;");
+	}
+	
+	public Dipendenti setPaziente(/*int id_utente,*/ Date data_nascita, String nome, String cognome,String codice_fiscale) {
+		return (Dipendenti) getAllDipendenti("Insert into Utenti(DATA_NASCITA,NOME,COGNOME,CODICE_FISCALE,ID_UTENTE) "
+				+ "values('"+data_nascita+"','"+nome+"','"+cognome+"','"+codice_fiscale+"')");
+	}
+	
+	
+	public Dipendenti getUtentiInCorso(String reparto/*da cambiare il nome reparto*/) {
+		return (Dipendenti) getAllDipendenti("Select pazienti.* "
+				+ "FROM Dipendenti, utenti, /***tabellaassociativaricoveri***/, /***tabellaassociativaprelievi***/, ricoveri, prelievi  "
+				+ "WHERE utenti.ID_UTENTE=prelievi.ID_UTENTE AND"
+				+ "utenti.ID_UTENTE=ricoveri.ID_UTENTE AND"
+				+ "/***tabellaassociativaricoveri***/.ID_RICOVERI=ricoveri.ID_RICOVERO AND"
+				+ "/***tabellaassociativaricoveri***/.ID_DIPENDENTI=DIPENDENTI.ID_DIPENDENTI AND"
+				+ "/***tabellaassociativaprelievi***/.ID_PRELIEVI=prelievi.ID_PRELIEVI AND"
+				+ "/***tabellaassociativaprelievi***/.ID_DIPENDENTI=DIPENDENTI.ID_DIPENDENTI AND"
+				+ "(DATA_ORA_FINE IS NULL OR"
+				+ "DATA_ORA_DIMISSIONE IS NULL)"
+				+ "GROUP BY utente;");
+	}
+	
+	public Dipendenti getUtenteReparto(String username) {
+		return (Dipendenti) getAllDipendenti("Select utenti.* "
+				+ "FROM utenti,dipendenti "
+				+ "WHERE dipendenti.reparti_ID_REPARTO=utenti.reparti_ID_REPARTO AND"
+				+ "dipendenti.USERNAME= '" +username +"'"
+				+ "GROUP BY utenti.ID_UTENTE");
+			
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
